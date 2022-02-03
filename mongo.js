b60fc05ead40f80633d4c9f6392becdf58d1a49b
Tom McDingus <mcdowelltomasz@gmail.com>
2022-02-03 21:17:29 +1300

    code to print all database entries to console

--- mongo.js
+++ mongo.js
@@ -19,13 +19,23 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
 
-note.save().then(result => {
-  console.log('note saved!')
+//NEW NOTE CREATION BELOW
+
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
